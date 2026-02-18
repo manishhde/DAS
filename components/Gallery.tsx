ed025048141878373f50a58077314ee3b98c7c8c
manishhde <aladdinbhai07@gmail.com>
2026-02-18 19:47:42 +0530

    Update Gallery.tsx

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -2,15 +2,15 @@
 import React, { useState } from 'react';
 
 const images = [
-  'gallery-1.jpg',
-  'gallery-2.jpg',
-  'gallery-3.jpg',
-  'gallery-4.jpg',
-  'gallery-5.jpg',
-  'gallery-6.jpg',
-  'gallery-7.jpg',
-  'gallery-8.jpg',
-  'gallery-9.jpg',
+  '/images/gallery-1.jpg',
+  '/images/gallery-2.jpg',
+  '/images/gallery-3.jpg',
+  '/images/gallery-4.jpg',
+  '/images/gallery-5.jpg',
+  '/images/gallery-6.jpg',
+  '/images/gallery-7.jpg',
+  '/images/gallery-8.jpg',
+  '/images/gallery-9.jpg',
 ];
 
 const Gallery: React.FC = () => {
